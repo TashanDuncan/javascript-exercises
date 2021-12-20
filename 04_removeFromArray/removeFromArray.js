25efc086aca4d04668da55bd0eb0c8ffd278fdfc
Tashan <tashanduncan1994@gmail.com>
2021-12-20 16:37:49 +0000

    complete 4th  exercise - remove from Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, rem1,rem2,rem3,rem4) {
+    let result = [];
+
+    arr.forEach(element => {
+        switch(element) {
+            case rem1:
+                break;
+            case rem2:
+                break;
+            case rem3:
+                break;
+            case rem4:
+                break;
+            default:
+                result.push(element)
+        }
+    });
+    return result
 
 };
 
