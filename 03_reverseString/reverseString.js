d5213a09a2a72aac99b2bc02dce882350ed3e2b9
Tashan <tashanduncan1994@gmail.com>
2021-12-20 16:11:34 +0000

    complete 3rd exercise - reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function(str) {
+    const splitString = str.split("")
+    
+    return splitString.reverse().join().toString().replace(/,/g,'') ;
 
+    
 };
 
 // Do not edit below this line
