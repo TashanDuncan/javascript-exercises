26c1285571bdcdc5d3d44830e142ebd5c8db8dc8
Tashan <tashanduncan1994@gmail.com>
2022-01-09 20:42:06 +0000

    complete calculator challenge

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((prev, curr) => prev + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((prev, curr) => prev * curr);
 };
 
-const power = function() {
-	
+const power = function (num, power) {
+  let result = num;
+  for (let i = 1; i < power; i++) {
+    result *= num;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let f = []
+  if (n == 0 || n == 1)
+    return 1;
+  if (f[n] > 0)
+    return f[n];
+  return f[n] = factorial(n-1) * n;
+
 };
 
 // Do not edit below this line
@@ -29,5 +40,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
