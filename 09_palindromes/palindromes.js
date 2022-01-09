9967b867097e51ce31fec28780473b30afd85a21
Tashan <tashanduncan1994@gmail.com>
2022-01-09 21:52:53 +0000

    complete palindromes challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  //create array of each letter removing punctuation
+  const noPunctuation = string
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s{2,}/g, " ");
 
+  const word = noPunctuation.split("").filter((entry) => entry.trim() != "");
+
+  console.log(word);
+
+  //create a new array which is reverse of the original.
+  const revWord = [...word].reverse();
+  console.log(revWord);
+
+  for (let i = 0; i < word.length; ++i) {
+    if (word[i] !== revWord[i]) return false;
+  }
+  return true
 };
+
+console.log(palindromes('ZZZZ car, a man, a maraca.'))
 
 // Do not edit below this line
 module.exports = palindromes;
