992bce8ecb77ae5e8172c5cd908e1d05ff77c4b6
Tashan <tashanduncan1994@gmail.com>
2022-01-10 12:01:12 +0000

    complete getthetitles challenge

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let result = [];
 
+    arr.map(book => result.push(book.title));
+    return result
 };
 
 // Do not edit below this line
