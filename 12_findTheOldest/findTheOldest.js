427cd0d7860b4bd1f9dd3705aec52f984a350065
Tashan <tashanduncan1994@gmail.com>
2022-01-10 12:32:33 +0000

    complete findtheoldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
 
+    const today = new Date();
+
+  const oldest = arr.sort(function (a, b) {
+    let lastperson;
+    let nextperson;
+    if (a.yearOfDeath) {
+      lastperson = a.yearOfDeath - a.yearOfBirth;
+    } else {
+        lastperson = today.getFullYear() - a.yearOfBirth;
+    }
+
+    if (b.yearOfDeath) {
+      nextperson = b.yearOfDeath - b.yearOfBirth;
+    } else{
+        nextperson = today.getFullYear() - b.yearOfBirth;
+    }
+
+    return lastperson > nextperson ? -1 : 1;
+  });
+  return oldest[0];
 };
 
 // Do not edit below this line
