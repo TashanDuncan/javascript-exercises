7cd323ea24a6442d4f7d74527e662be5180ad46b
Tashan <tashanduncan1994@gmail.com>
2022-01-10 11:32:46 +0000

    complete fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+function fibonacci(num) {
+    if(num === 0) return 0;
+    if(num < 0) return "OOPS";
 
-};
+let a = 0;
+let b = 1;
+
+for(let i = 1; i < num; i++) {
+    let temp = b;
+
+    b = a + b;
+    a = temp;
+}
+
+  return b;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
